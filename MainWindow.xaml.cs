a802a37cf5811f41b1808e4419f42abce9a879c7
sader.peron@mail.ru <sader.peron@mail.ru>
2022-06-24 21:58:02 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool whitebg = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+           
+          if (this.whitebg )
+          {
+                cvet.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                this.whitebg = false;
+                    
+          }
+          else 
+            {
+                cvet.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.whitebg = true;
+            }
+           
+          
+
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Брюнцева Анастасия\nГруппа ИСП-32\nСпасибо за внимание!");
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window2 w = new Window2();
+            w.Show();
+           
+        }
+       
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab2
+{
+	/// <summary>
+	/// Логика взаимодействия для Window2.xaml
+	/// </summary>
+	public partial class Window2 : Window
+	{
+		double a;
+		
+		public Window2()
+		{
+			InitializeComponent();
+		}
+
+		private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+		{
+			abc.DefaultDrawingAttributes.Color = Colors.Red;
+		}
+
+		private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+		{
+			abc.DefaultDrawingAttributes.Color = Colors.Green;
+		}
+
+		private void ComboBoxItem_Selected_2(object sender, RoutedEventArgs e)
+		{
+			abc.DefaultDrawingAttributes.Color = Colors.Blue;
+		}
+
+		private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (abc != null)
+			{
+				a = be.Value;
+				be.Value = Math.Round(e.NewValue, 0);
+				abc.DefaultDrawingAttributes.Width = a;
+				abc.DefaultDrawingAttributes.Height = a;
+				val.Text = be.Value.ToString();
+			}
+		}
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+			abc.EditingMode = InkCanvasEditingMode.EraseByPoint;
+		}
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+			abc.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+			abc.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        
+    }
+}
